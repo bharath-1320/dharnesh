d597e881e72f245eb5802558c04fb1e7980eb140
bharath-1320 <160496801+bharath-1320@users.noreply.github.com>
2025-07-04 02:32:20 +0530

    Update kube-deploy.sh

--- backend/kube-deploy.sh
+++ backend/kube-deploy.sh
@@ -16,16 +16,20 @@ gcloud container clusters get-credentials "${CLUSTER_NAME}" --zone="${LOCATION}"
 
 echo "*** Creating deployment YAML files using envsubst ***"
 mkdir -p "./output"
-env IMAGE_URL="${IMAGE_URL}" PROJECT_ID="${PROJECT_ID}" CLUSTER_NAME="${CLUSTER_NAME}" LOCATION="${LOCATION}" \
 envsubst < "../k8s/Backend/backend-deployment.tmpl" > "./output/backend-deployment.yaml"
 env IMAGE_URL="${IMAGE_URL}" \
 envsubst < "../k8s/Backend/backend-service.tmpl" > "./output/backend-service.yaml"
 env IMAGE_URL="${IMAGE_URL}" \
-envsubst < "../k8s/Backend/ingress.tmpl" > "./output/ingress.yaml"
+# env IMAGE_URL="${IMAGE_URL}" PROJECT_ID="${PROJECT_ID}" CLUSTER_NAME="${CLUSTER_NAME}" LOCATION="${LOCATION}" \
+# envsubst < "../k8s/Backend/backend-deployment.tmpl" > "./output/backend-deployment.yaml"
+# env IMAGE_URL="${IMAGE_URL}" \
+# envsubst < "../k8s/Backend/backend-service.tmpl" > "./output/backend-service.yaml"
+# env IMAGE_URL="${IMAGE_URL}" \
+# envsubst < "../k8s/Backend/ingress.tmpl" > "./output/ingress.yaml"
 
 echo "*** Deploying Docker container and setting up the service and ingress ***"
 kubectl apply -f "./output/backend-deployment.yaml" --validate=false --insecure-skip-tls-verify=true
 kubectl apply -f "./output/backend-service.yaml" --validate=false --insecure-skip-tls-verify=true
-kubectl apply -f "./output/ingress.yaml" --validate=false --insecure-skip-tls-verify=true
+# kubectl apply -f "./output/ingress.yaml" --validate=false --insecure-skip-tls-verify=true
 
 echo "Deployment commands executed successfully."
