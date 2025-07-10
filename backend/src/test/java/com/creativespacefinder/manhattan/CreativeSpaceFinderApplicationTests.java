c0dabe1df7ffbcfe90d0de04db51d11187671f00
bharath-1320 <160496801+bharath-1320@users.noreply.github.com>
2025-07-11 03:18:57 +0530

    Update CreativeSpaceFinderApplicationTests.java

--- backend/src/test/java/com/creativespacefinder/manhattan/CreativeSpaceFinderApplicationTests.java
+++ backend/src/test/java/com/creativespacefinder/manhattan/CreativeSpaceFinderApplicationTests.java
@@ -2,6 +2,7 @@ package com.creativespacefinder.manhattan;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles; // This is the missing import
 
 @SpringBootTest
 @ActiveProfiles("test")
