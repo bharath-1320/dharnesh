ba3957e7b7dd471d20a2379265e2e26d123d68d5
bharath-1320 <160496801+bharath-1320@users.noreply.github.com>
2025-07-11 03:23:28 +0530

    Update CreativeSpaceFinderApplicationTests.java

--- backend/src/test/java/com/creativespacefinder/manhattan/CreativeSpaceFinderApplicationTests.java
+++ backend/src/test/java/com/creativespacefinder/manhattan/CreativeSpaceFinderApplicationTests.java
@@ -1,15 +1,23 @@
 package com.creativespacefinder.manhattan;
 
+import com.creativespacefinder.manhattan.service.WeatherForecastService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles; // This is the missing import
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@TestPropertySource(properties = {
+    "openweather.api-key=test-api-key"
+})
 class CreativeSpaceFinderApplicationTests {
+
+    @MockBean
+    private WeatherForecastService weatherForecastService;
 
     @Test
     void contextLoads() {
     }
-
 }
